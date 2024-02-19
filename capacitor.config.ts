ba6ceaaad23724d24b8616ee71d672c7e827a9c1
Jeric <jmnarte@up.edu.ph>
2024-02-20 00:38:42 +0800

    style: apply lint

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,12 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.studentfinancetracker.app',
-  appName: 'Student Finance Tracker',
-  webDir: 'build',
-  server: {
-    androidScheme: 'https'
-  }
+    appId: 'com.studentfinancetracker.app',
+    appName: 'Student Finance Tracker',
+    webDir: 'build',
+    server: {
+        androidScheme: 'https',
+    },
 };
 
 export default config;
