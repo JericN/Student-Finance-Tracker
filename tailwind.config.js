e4661510b75e15b2d7ea0e448cd5c818a8fb9e7a
Jeric <jmnarte@up.edu.ph>
2024-02-20 23:22:32 +0800

    feat: add tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,28 @@ export default {
         join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}'),
     ],
     theme: {
-        extend: {},
+        extend: {
+            fontSize: {
+                '3xs': '6px',
+                '2xs': '8px',
+            },
+            scale: {
+                101: '1.01',
+            },
+            colors: {
+                income: '#244b8f',
+                expense: '#5e1b1b',
+                neutral: '#103d1a',
+            },
+            gridTemplateColumns: {
+                thead: '20px minmax(50px, 1fr) 50px 50px',
+                tbody: '20px 70px minmax(50px, 1fr) 50px 50px',
+            },
+            borderRadius: {
+                sm: '4px',
+                xs: '2px',
+            },
+        },
     },
     plugins: [
         forms,
