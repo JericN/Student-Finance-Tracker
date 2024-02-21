d7331f65b7f257fc5c4821ae2ef4d2a83b330ba4
Jeric <jmnarte@up.edu.ph>
2024-02-21 10:10:44 +0800

    feat: add helper function for transforming currency

---
+++ src/lib/funcs/funcs.ts
@@ -0,0 +1,4 @@
+const currencyOptions = { style: 'currency', currency: 'PHP', maximumFractionDigits: 0 };
+export function currency(value: number) {
+    return value.toLocaleString('en-US', currencyOptions);
+}
