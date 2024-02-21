bd67136813551b8c5c2d936c6f7c512320fc71f7
Jeric <jmnarte@up.edu.ph>
2024-02-21 23:05:20 +0800

    fix(ui): prevent focus style on tap

--- src/lib/models/types.ts
+++ src/lib/models/types.ts
@@ -19,18 +19,30 @@ export const Record = array(
 export type Record = Output<typeof Record>;
 
 export const colors = {
-    slate: 'bg-slate-700 hover:bg-slate-600',
-    red: 'bg-red-900 hover:bg-red-800',
-    amber: 'bg-amber-900 hover:bg-amber-800',
-    green: 'bg-green-900 hover:bg-green-800',
-    teal: 'bg-teal-900 hover:bg-teal-800',
-    cyan: 'bg-cyan-900 hover:bg-cyan-800',
-    blue: 'bg-blue-900 hover:bg-blue-800',
-    indigo: 'bg-indigo-900 hover:bg-indigo-800',
-    purple: 'bg-purple-900 hover:bg-purple-800',
-    fuchsia: 'bg-fuchsia-900 hover:bg-fuchsia-800',
-    rose: 'bg-rose-900 hover:bg-rose-800',
-    surface: 'bg-surface-900 hover:bg-surface-800',
+    slate: 'bg-slate-700 active:bg-slate-600',
+    red: 'bg-red-900 active:bg-red-800',
+    amber: 'bg-amber-900 active:bg-amber-800',
+    green: 'bg-green-900 active:bg-green-800',
+    teal: 'bg-teal-900 active:bg-teal-800',
+    cyan: 'bg-cyan-900 active:bg-cyan-800',
+    blue: 'bg-blue-900 active:bg-blue-800',
+    indigo: 'bg-indigo-900 active:bg-indigo-800',
+    purple: 'bg-purple-900 active:bg-purple-800',
+    fuchsia: 'bg-fuchsia-900 active:bg-fuchsia-800',
+    rose: 'bg-rose-900 active:bg-rose-800',
+    surface: 'bg-surface-700 active:bg-surface-600',
 };
 
 export type Colors = keyof typeof colors;
+
+export enum days {
+    Sun,
+    Mon,
+    Tue,
+    Wed,
+    Thu,
+    Fri,
+    Sat,
+}
+
+export type Days = (typeof days)[number];
