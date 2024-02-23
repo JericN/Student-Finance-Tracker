337fee1209562a5aaa7413394ba284155460b276
Jeric <jmnarte@up.edu.ph>
2024-02-23 21:54:26 +0800

    feat(dashboard): render expenses chart to dashboard

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import Budget from './Budget.svelte';
+    import Chart from './Chart.svelte';
     import Wallets from './Wallets.svelte';
 
     const balance = 1200;
@@ -9,9 +10,19 @@
         Cash: 1200,
         GCash: 500,
     };
+    const expenses = {
+        Mon: 200,
+        Tue: 320,
+        Wed: 210,
+        Thu: 190,
+        Fri: 200,
+        Sat: 340,
+        Sun: 620,
+    };
 </script>
 
 <div class="flex h-full flex-col items-center justify-between p-10">
     <Budget {balance} {maxBalance} />
+    <Chart dataset={expenses} />
     <Wallets {wallets} />
 </div>
---
+++ src/routes/dashboard/Chart.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import LineChart from '$lib/charts/LineChart.svelte';
+
+    // TODO: should fetch the most recent transactions
+    export let dataset: Record<string, number>;
+</script>
+
+<div class="w-full max-w-sm">
+    <div class="text-center font-bold">EXPENSES</div>
+    <LineChart {dataset} />
+</div>
