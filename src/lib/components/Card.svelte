f965cdaa080989a07037de5e2051c63b35ed7167
Soda <62868605+JericN@users.noreply.github.com>
2024-02-23 15:29:13 +0800

    refactor: add props to Card

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -2,12 +2,13 @@
     import { type Colors, colors } from '$lib/models/types';
 
     export let width = 'w-full';
-    export let color = 'surface' as Colors;
+    export let accent = 'surface' as Colors;
+    export let background = 'bg-surface-50';
 </script>
 
-<div class={width}>
-    <div class={`border-default translate-x-1.5 translate-y-1.5 rounded-xl ${colors[color]}`}>
-        <div class="border-default -translate-x-1.5 -translate-y-1 scale-101 rounded-xl bg-surface-50">
+<div class={`p-2 ${width}`}>
+    <div class={`border-default rounded-xl ${colors[accent]}`}>
+        <div class={`border-default -translate-x-1.5 -translate-y-1 scale-101 rounded-xl ${background}`}>
             <slot />
         </div>
     </div>
