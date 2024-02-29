a6456c67eb44a468bb768912c7815915add418a4
Jeric <jmnarte@up.edu.ph>
2024-02-29 20:52:41 +0800

    fix: add redirect from root

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -6,10 +6,10 @@ import { get } from 'svelte/store';
 import { redirect } from '@sveltejs/kit';
 import { session } from '$lib/store/user';
 
-export async function load({ parent, url }) {
-    await parent();
+export function load({ url }) {
     const user = get(session);
     const authPath = /^\/auth\/.*/.test(url.pathname);
     if (!user.auth && !authPath) redirect(302, '/auth/login/');
     if (user.auth && authPath) redirect(302, '/user/dashboard/');
+    if (url.pathname === '/') redirect(302, '/auth/login/');
 }
