889075d209759853d9c9dd848a96b433ba6b4f78
Jeric <jmnarte@up.edu.ph>
2024-03-01 00:59:36 +0800

    refactor(transactions): use components

--- src/routes/user/transactions/(popup)/edit/Description.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-    export let description: string;
-</script>
-
-<div class="mt-5">
-    <textarea bind:value={description} class="textarea" rows="2" placeholder="Notes" />
-</div>
