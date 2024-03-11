4313a273a6de5fbb084ae45384ccf1898d2fa220
AlJf-the-Coder <agjafri@up.edu.ph>
2024-03-12 00:19:42 +0800

    feat: add content to users page

--- src/routes/user/users/+page.svelte
+++ src/routes/user/users/+page.svelte
@@ -1,5 +1,53 @@
-<script>
-    import Placeholder from '$lib/components/Placeholder.svelte';
+<script lang='ts'>
+    // import Placeholder from '$lib/components/Placeholder.svelte';
+    import SettingsCard from './SettingsCard.svelte'
+    import { LightSwitch } from '@skeletonlabs/skeleton';
+    import { goto } from '$app/navigation';
+    import { session } from '$lib/store/user';
+    import { testFetch } from '$lib/api/test';
+
+    import { Icon } from '@steeze-ui/svelte-icon';
+
+    let id = 1;
+    function getData(id: number) {
+        return testFetch(id);
+    }
+
+    // eslint-disable-next-line init-declarations
+
 </script>
 
-<Placeholder title="Users Page" />
+<!-- <Placeholder title="Users Page" /> -->
+
+<div class="flex h-full flex-col items-center justify-center justify-items-start gap-10">
+    <div class="text-xl font-bold">User Page</div>
+    <div class="text-secondary-800-100-token font-bold uppercase underline">
+        <p>UserName</p>
+    </div>
+
+    <SettingsCard title="Dark Mode">
+        <LightSwitch class={'select-none'} />
+    </SettingsCard>
+
+    <SettingsCard title="Wallets">
+        <div>Add/remove Wallet</div>
+        <div>Edit Wallet</div>
+    </SettingsCard>
+
+    <SettingsCard title="Wishlist">
+        <div>Add/remove Wishlist Item</div>
+        <div>Edit Wishlist Item</div>
+    </SettingsCard>
+    
+                
+    <button
+        class="variant-filled btn"
+        on:click={() => {
+            $session.auth = false;
+            goto('/auth/login');
+        }}
+    >
+        Logout
+    </button>
+</div>
+
---
+++ src/routes/user/users/SettingsCard.svelte
@@ -0,0 +1,30 @@
+<script lang='ts'>
+    import Card from '$lib/components/Card.svelte';
+    export let title: string;
+
+    let deviceWidth = window.innerWidth;
+    window.addEventListener('resize', () => {
+        deviceWidth = window.innerWidth;
+    });
+
+    function update(deviceWidth: number): string {
+        switch (true) {
+            case deviceWidth > 440:
+                return "w-2/4";
+            default:
+                return "w-3/4";
+        }
+    }
+
+    $: cardWidth = update(deviceWidth);
+</script>
+<Card width={`${cardWidth} flex-none`} padding="px-6">
+    <div class="justify-between text-dark">
+        <div class="flex flex-col items-start">
+            <div class="text-2x1 font-bold">{title}</div>
+        </div>
+        <div class="flex flex-col my-auto text-2xl items-end">
+            <slot></slot>
+        </div>
+    </div>
+</Card>
