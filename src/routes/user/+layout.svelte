4bca834c2857a66d61b63e1a0d1332122765fa4d
Jeric <jmnarte@up.edu.ph>
2024-03-12 10:23:44 +0800

    fix: prefer single layout

--- src/routes/user/+layout.svelte
+++ src/routes/user/+layout.svelte
@@ -1,9 +1,23 @@
 <script>
     import { AppShell } from '@skeletonlabs/skeleton';
+    import Header from '$lib/components/Header.svelte';
     import NavBar from '$lib/components/NavBar.svelte';
+    import { page } from '$app/stores';
+
+    $: current = $page.url.pathname;
+    $: showHeader = current === '/user/transactions/create/' || current === '/user/transactions/edit/';
 </script>
 
 <AppShell>
+    <div slot="header">
+        {#if showHeader}
+            <Header />
+        {/if}
+    </div>
     <slot />
-    <NavBar slot="footer" />
+    <div slot="footer">
+        {#if !showHeader}
+            <NavBar />
+        {/if}
+    </div>
 </AppShell>
