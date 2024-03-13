245e50e8ca447850339b8c514c1bbe778fe24575
Jeric <jmnarte@up.edu.ph>
2024-03-14 04:28:44 +0800

    fix: expose padding of button component

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
     export let accent = 'bg-primary-900';
+    export let padding = '3px';
 </script>
 
 <div class={`${accent} border-default rounded-2xl`}>
     <button
-        class="border-default -translate-x-1 -translate-y-1 scale-105 rounded-2xl bg-surface-100 p-2 active:translate-x-0 active:translate-y-0"
+        class={`border-default -translate-x-1 -translate-y-1 scale-105 rounded-2xl bg-surface-100 p-2 text-dark active:translate-x-0 active:translate-y-0 ${padding}`}
         on:click
     >
         <slot />
