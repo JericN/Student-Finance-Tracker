6547b4c9d0edb007943a2eebebb511d8d0fa09ed
Jeric <jmnarte@up.edu.ph>
2024-03-13 22:07:44 +0800

    feat: hook create transaction to backend

--- src/routes/user/transactions/(popup)/create/+page.svelte
+++ src/routes/user/transactions/(popup)/create/+page.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
     import * as createStore from '$lib/store/creating';
-    import { type ToastSettings, getToastStore } from '@skeletonlabs/skeleton';
-    import { pick, safeParse } from 'valibot';
+    import { error, success } from '$lib/funcs/toast';
+    import { parse, pick, safeParse } from 'valibot';
     import Amount from '$lib/components/transaction/Amount.svelte';
     import Button from '$lib/components/Button.svelte';
     import Calendar from '$lib/components/transaction/Calendar.svelte';
     import Card from '$lib/components/Card.svelte';
     import Category from '$lib/components/transaction/Category.svelte';
     import Description from '$lib/components/transaction/Description.svelte';
-    import { Transaction } from '$lib/models/types';
+    import { Record } from '$lib/models/types';
     import Type from '$lib/components/transaction/Type.svelte';
     import Wallet from '$lib/components/transaction/Wallet.svelte';
+    import { addTransaction } from '$lib/firebase/database';
+    import { getToastStore } from '@skeletonlabs/skeleton';
     import { goto } from '$app/navigation';
 
     const toastStore = getToastStore();
@@ -31,43 +33,25 @@
     // TODO: move accounts to a store
     const wallets = ['Bank', 'Cash', 'GCash', 'Shared'];
 
-    function t(message: string): ToastSettings {
-        return {
-            message: message,
-            timeout: 2000,
-            background: 'variant-filled-error',
-        };
-    }
+    async function submit() {
+        const properties: (keyof Record)[] = ['type', 'amount', 'date', 'category', 'wallet', 'description'];
 
-    // TODO: send post request to server
-    function create() {
-        if (!safeParse(pick(Transaction, ['amount']), { amount: $cstore.amount }).success) {
-            toastStore.trigger(t('Invalid amount'));
-            return;
+        for (const property of properties) {
+            const result = safeParse(pick(Record, [property]), { [property]: $cstore[property] });
+            if (!result.success) {
+                toastStore.trigger(error(`Invalid ${property}`));
+                return;
+            }
         }
-        if (!safeParse(pick(Transaction, ['date']), { date: $cstore.date }).success) {
-            toastStore.trigger(t('Invalid date'));
-            return;
+
+        try {
+            await addTransaction(parse(Record, $cstore));
+            goto('/user/transactions');
+            cstore.reset();
+            toastStore.trigger(success('Transaction added'));
+        } catch (_) {
+            toastStore.trigger(error('Failed to add transaction'));
         }
-        if (!safeParse(pick(Transaction, ['category']), { category: $cstore.category }).success) {
-            toastStore.trigger(t('Invalid category'));
-            return;
-        }
-        if (!safeParse(pick(Transaction, ['wallet']), { wallet: $cstore.wallet }).success) {
-            toastStore.trigger(t('Invalid wallet'));
-            return;
-        }
-        if (!safeParse(pick(Transaction, ['description']), { description: $cstore.description }).success) {
-            toastStore.trigger(t('Invalid description'));
-            return;
-        }
-        if (!safeParse(pick(Transaction, ['type']), { type: $cstore.type }).success) {
-            toastStore.trigger(t('Invalid type'));
-            return;
-        }
-        goto('/user/transactions');
-        cstore.reset();
-        toastStore.trigger(t('Transaction saved'));
     }
 </script>
 
@@ -82,7 +66,7 @@
         </div>
         <Description bind:description={$cstore.description} />
     </Card>
-    <Button on:click={() => create()}>
+    <Button on:click={() => submit()}>
         <span class="px-4 font-bold text-dark"> SAVE </span>
     </Button>
 </div>
