0e79c269dfd92d83e5b79ce878f120ac624cd7b5
Vince <vcsantos4@up.edu.ph>
2024-03-14 23:40:38 +0800

    feat: validation function for auth

---
+++ src/lib/validation/auth.ts
@@ -0,0 +1,21 @@
+
+export function validateUsername(input: string): boolean {
+    const re = /[0-9A-Za-z]/;
+    const filtered = input.split("").filter(e => !re.test(e));
+
+    const validChars =  filtered.length === 0;
+    const validLen = input.length <= 12;
+
+    return validChars && validLen;
+}
+
+
+
+export function validatePassword(input: string): boolean {
+    const re = /[0-9]/;
+
+    const hasNumber = re.test(input);
+    const validLen = input.length <= 20;
+
+    return hasNumber && validLen;
+}
