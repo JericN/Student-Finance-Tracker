f37453d25f120c8cab1c7a91d4d4459bd29d8995
Jeric <jmnarte@up.edu.ph>
2024-03-14 23:02:42 +0800

    feat(user): hook create template to database

--- src/lib/firebase/database.ts
+++ src/lib/firebase/database.ts
@@ -1,4 +1,4 @@
-import { type Record, Transaction } from '$lib/models/types';
+import { type Record, type TemplateForms, Transaction } from '$lib/models/types';
 import { addDoc, collection, deleteDoc, doc, getDocs, serverTimestamp, setDoc } from 'firebase/firestore';
 import type { User } from 'firebase/auth';
 import { db } from '$lib/firebase/firebase.client';
@@ -66,5 +66,17 @@ export async function getTransactions(): Promise<Transaction[]> {
         return transactions;
     } catch (e) {
         throw new Error('Failed retrieving transactions');
+    }
+}
+
+// This function is used to create a new template
+export async function addTemplate(data: TemplateForms) {
+    const path = `UserData/${session.uid()}/transaction-templates`;
+    const payload = { ...data, createdAt: serverTimestamp(), updatedAt: serverTimestamp() };
+
+    try {
+        await addDoc(collection(db, path), payload);
+    } catch (e) {
+        throw new Error('Failed adding template');
     }
 }
