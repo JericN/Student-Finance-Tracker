5cd752fc61f3ce4803e3c7ddfe78e97bf086f8fe
Soda <62868605+JericN@users.noreply.github.com>
2024-03-27 17:41:46 +0800

    refactor: improve type button component

--- src/lib/components/CardButton.svelte
+++ src/lib/components/CardButton.svelte
@@ -2,15 +2,15 @@
     import { twMerge } from 'tailwind-merge';
 
     export let accent = 'bg-primary-900';
-    export let padding = 'px-4 py-2';
-    export let width = 'w-fit';
     export let background = 'bg-surface-100';
+    export let padding = '';
+    export let width = '';
     export let layout = '';
     export let font = '';
 </script>
 
-<div class={twMerge('w-full p-2', width)}>
-    <div class={twMerge('border-default translate-x-1', accent)}>
+<div class={twMerge('w-full max-w-screen-sm p-2', width)}>
+    <div class={`border-default translate-x-1 ${accent}`}>
         <button
             class={twMerge(
                 'border-default flex w-full -translate-x-1 -translate-y-1 scale-101 items-center justify-center p-4 active:translate-x-0 active:translate-y-0',
---
+++ src/lib/components/index.js
@@ -0,0 +1,4 @@
+export { default as Button } from '$lib/components/SimpleButton.svelte';
+export { default as CardButton } from '$lib/components/CardButton.svelte';
+export { default as Day } from '$lib/components/Day.svelte';
+export { default as Card } from '$lib/components/Card.svelte';
