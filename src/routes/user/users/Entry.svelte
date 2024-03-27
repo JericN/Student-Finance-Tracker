a9f8870955069f38925dcc9a0c742b670bd71ed9
Soda <62868605+JericN@users.noreply.github.com>
2024-03-27 18:25:15 +0800

    fix: use `buttonCard` component in users page

--- src/routes/user/users/Entry.svelte
+++ src/routes/user/users/Entry.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { Card } from '$lib/components/modules';
+    import { CardButton } from '$lib/components/modules';
     import { goto } from '$app/navigation';
 
     export let title: string;
@@ -10,13 +10,13 @@
     }
 </script>
 
-<Card width="max-w-screen-sm" padding="px-6">
-    <button class="w-full justify-between text-dark" on:click={redir}>
+<CardButton on:click={redir}>
+    <div class="w-full text-dark">
         <div class="flex flex-col items-start">
             <div class="text-2x1 font-bold">{title}</div>
         </div>
         <div class="my-auto flex flex-col items-end text-sm">
             <slot />
         </div>
-    </button>
-</Card>
+    </div>
+</CardButton>
