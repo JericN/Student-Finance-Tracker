8a8368a1791f0962a163eaac79a57a1d773ce007
Soda <62868605+JericN@users.noreply.github.com>
2024-04-18 19:34:45 +0800

    fix: remove unused types

--- src/lib/models/types.ts
+++ src/lib/models/types.ts
@@ -1,22 +1,4 @@
-import { type Output, array, keyof, number, object, record, safeInteger, string } from 'valibot';
-import { Transaction } from './sft';
-
-export const NameCount = record(number([safeInteger()]));
-export type NameCount = Output<typeof NameCount>;
-
-export const NameNumber = record(number());
-export type NameNumber = Output<typeof NameNumber>;
+import { type Output, number, record } from 'valibot';
 
 export const StackedData = record(record(number()));
 export type StackedData = Output<typeof StackedData>;
-
-// export const StackedDate = array(object({ group: string(), x: date(), y: number() }));
-// export type StackedDate = Output<typeof StackedDate>;
-// type TS = {
-//     data: Transaction[];
-//     group: keyof Transaction;
-//     range: string;
-// };
-
-export const AnalysisParams = object({ data: array(Transaction), group: keyof(Transaction), range: string() });
-export type AnalysisParams = Output<typeof AnalysisParams>;
