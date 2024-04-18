7d2bf98769c82f488f9c7d56e518373edca52f1e
Jeric <jmnarte@up.edu.ph>
2024-04-19 02:49:53 +0800

    feat: implement radio component

---
+++ src/lib/components/forms/Radio.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { RadioGroup, RadioItem } from '@skeletonlabs/skeleton';
+
+    export let select: string;
+    export let list: string[];
+</script>
+
+<RadioGroup
+    border="border-2 border-primary-900"
+    rounded="rounded-xl"
+    active="bg-primary-900 text-white"
+    hover="hover:variant-soft-primary"
+>
+    {#each list as item}
+        <RadioItem bind:group={select} name={item} value={item}>{item}</RadioItem>
+    {/each}
+</RadioGroup>
--- src/lib/components/forms/index.js
+++ src/lib/components/forms/index.js
@@ -6,3 +6,4 @@ export { default as Name } from '$lib/components/forms/Name.svelte';
 export { default as Type } from '$lib/components/forms/Type.svelte';
 export { default as Wallet } from '$lib/components/forms/Wallet.svelte';
 export { default as Icon } from '$lib/components/forms/Icon.svelte';
+export { default as Radio } from '$lib/components/forms/Radio.svelte';
