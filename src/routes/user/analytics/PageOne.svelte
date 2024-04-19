9bf51d561e7a984fbdbd246d94b45ad80560dbd4
Jeric <jmnarte@up.edu.ph>
2024-04-19 11:48:48 +0800

    feat: add transaction summary

--- src/routes/user/analytics/PageOne.svelte
+++ src/routes/user/analytics/PageOne.svelte
@@ -2,10 +2,17 @@
     import CategoryList from './CategoryList.svelte';
     import type { NameNumber } from '$lib/models/types';
     import PieChart from '$lib/charts/PieChart.svelte';
+    import Summary from './Summary.svelte';
 
     export let pie: { income: NameNumber; expense: NameNumber };
     export let currentType: string;
+
+    $: totalIncome = Object.values(pie.income).reduce((acc, num) => acc + num, 0);
+    $: totalExpense = Object.values(pie.expense).reduce((acc, num) => acc + num, 0);
 </script>
 
-<PieChart dataset={currentType === 'Income' ? pie.income : pie.expense} />
-<CategoryList data={pie} {currentType} />
+<div class="flex flex-col items-center gap-2">
+    <Summary income={totalIncome} expenses={totalExpense} balance={totalIncome - totalExpense} />
+    <PieChart dataset={currentType === 'Income' ? pie.income : pie.expense} />
+    <CategoryList data={pie} {currentType} />
+</div>
