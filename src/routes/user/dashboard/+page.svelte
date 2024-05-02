a92213481b4a5a4508c960511ca763bb68ce6506
BerryDepressed123 <134140375+BerryDepressed123@users.noreply.github.com>
2024-05-02 19:36:46 +0800

    dashboard
    linking it to dashboard

--- src/routes/user/dashboard/+page.svelte
+++ src/routes/user/dashboard/+page.svelte
@@ -3,12 +3,13 @@
     import Chart from './Chart.svelte';
     import Wallets from './Wallets.svelte';
 
-    const balance = 1200;
-    const maxBalance = 2500;
+    import { getBudgetPrefStore } from '$lib/store/database';
+
+    const budgetPrefStore = getBudgetPrefStore();
 </script>
 
 <div class="flex h-full flex-col items-center justify-between p-10">
-    <Budget {balance} {maxBalance} />
+    <Budget balance={$budgetPrefStore[0].amount} maxBalance={$budgetPrefStore[0].goal} />
     <Chart />
     <Wallets />
 </div>
