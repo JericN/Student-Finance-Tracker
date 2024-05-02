6ca7a62cf79f050611ab1a394ff158878e13d89c
Jeric <jmnarte@up.edu.ph>
2024-05-02 23:10:59 +0800

    fix: remove add budget

--- src/routes/user/users/budget-pref/create/+page.ts
+++
@@ -1,9 +0,0 @@
-import { get } from 'svelte/store';
-import { goto } from '$app/navigation';
-import { session } from '$lib/store/session';
-
-export async function load({ parent }) {
-    await parent();
-    const { loggedIn } = get(session);
-    if (!loggedIn) goto('/auth/login');
-}
