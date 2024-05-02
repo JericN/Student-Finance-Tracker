29e4542b7bb99312d3c565201dfee45da90a2c88
Jeric <jmnarte@up.edu.ph>
2024-05-02 22:51:51 +0800

    fix: await for database data

--- src/routes/user/dashboard/+page.svelte
+++ src/routes/user/dashboard/+page.svelte
@@ -6,10 +6,11 @@
     import { getBudgetPrefStore } from '$lib/store/database';
 
     const budgetPrefStore = getBudgetPrefStore();
+    $: current = $budgetPrefStore.length !== 0 ? $budgetPrefStore[0] : { amount: 0, goal: 0 };
 </script>
 
 <div class="flex h-full flex-col items-center justify-between p-10">
-    <Budget balance={$budgetPrefStore[0].amount} maxBalance={$budgetPrefStore[0].goal} />
+    <Budget balance={current.amount} maxBalance={current.goal} />
     <Chart />
     <Wallets />
 </div>
